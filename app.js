39ad8c8a58a5924c919fe32be751b196f128c7d4
Veronika Kostenko PC <veronikostenko@gmail.com>
2022-04-10 21:40:37 +0300

    create add note option

--- app.js
+++ app.js
@@ -53,11 +53,18 @@ const startData = [
   },
 ];
 
+let data = [startData];
+
 // Selectors
 const tableHeader = document.querySelector(".table-container");
 const tableButtons = document.querySelector(".table-container");
 const headerButton = document.querySelector(".header-button");
 const closePopupButton = document.querySelector(".fa-square-xmark");
+const saveNoteButton = document.querySelector(".save-note-button");
+const inputArea = document.querySelector(".input-area");
+const noteName = document.querySelector("input");
+const noteContent = document.querySelector("textarea");
+const category = document.querySelector(".choose-category");
 
 // Functions
 
@@ -81,68 +88,81 @@ const categoryIcon = (category) => {
   return output;
 };
 
+const showDataNode = (node) => {
+  const tableRow = document.createElement("div");
+  tableRow.classList.add("table-row");
+  const name = document.createElement("div");
+  name.classList.add("name");
+  name.innerHTML = `${categoryIcon(node.category)}
+  <b>${node.name}</b>`;
+
+  const created = document.createElement("div");
+  created.classList.add("created");
+  created.innerText = `${node.created}`;
+
+  const category = document.createElement("div");
+  category.classList.add("category");
+  category.innerText = `${node.category}`;
+
+  const content = document.createElement("div");
+  content.classList.add("content");
+  content.innerText = `${node.content}`;
+
+  const dates = document.createElement("div");
+  dates.classList.add("dates");
+  dates.innerText = `${node.dates}`;
+
+  const tableButtons = document.createElement("div");
+  tableButtons.classList.add("table-buttons");
+  tableButtons.innerHTML = `
+  <button>
+      <i class="fa-solid fa-pen"></i>
+  </button>
+  <button>
+      <i class="fas fa-box-archive"></i>
+  </button>
+  <button>
+      <i class="fas fa-trash"></i>
+  </button>`;
+
+  tableRow.append(name, created, category, content, dates, tableButtons);
+  tableHeader.appendChild(tableRow);
+};
 // SHOW DATA FROM START
 const showStartData = (startData) => {
   startData.forEach((data) => {
-    const tableRow = document.createElement("div");
-    tableRow.classList.add("table-row");
-    const name = document.createElement("div");
-    name.classList.add("name");
-    name.innerHTML = `${categoryIcon(data.category)}
-    <b>${data.name}</b>`;
-
-    const created = document.createElement("div");
-    created.classList.add("created");
-    created.innerText = `${data.created}`;
-
-    const category = document.createElement("div");
-    category.classList.add("category");
-    category.innerText = `${data.category}`;
-
-    const content = document.createElement("div");
-    content.classList.add("content");
-    content.innerText = `${data.content}`;
-
-    const dates = document.createElement("div");
-    dates.classList.add("dates");
-    dates.innerText = `${data.dates}`;
-
-    const tableButtons = document.createElement("div");
-    tableButtons.classList.add("table-buttons");
-    tableButtons.innerHTML = `
-    <button>
-        <i class="fa-solid fa-pen"></i>
-    </button>
-    <button>
-        <i class="fas fa-box-archive"></i>
-    </button>
-    <button>
-        <i class="fas fa-trash"></i>
-    </button>`;
-
-    tableRow.append(name, created, category, content, dates, tableButtons);
-    tableHeader.appendChild(tableRow);
+    showDataNode(data);
   });
 };
 
-const deleteNote = (e) => {
+const iterateNote = (e) => {
   e.preventDefault();
   let item = e.target;
-  console.log("hey, we delele it", e.target.classList);
   const tableRow = item.closest(".table-row");
+
+  // DELETE NOTE
   if (
     item.classList[1] === "fa-trash" &&
     tableRow.classList[0] === "table-row"
   ) {
     tableRow.remove();
   }
+
+  // EDIT NOTE
+  else if (item.classList[1] === "fa-pen") {
+    inputArea.style.display = "flex";
+    console.log(e.target.closest(".table-row").children[0].innerText);
+    noteName.value = e.target.closest(".table-row").children[0].innerText;
+    noteContent.value = e.target.closest(".table-row").children[3].innerText;
+    category.value = e.target.closest(".table-row").children[2].innerText;
+
+    console.log(tableHeader.childNodes);
+    let data = tableHeader.childNodes;
+    console.log(typeof data);
+  }
 };
 
-const inputArea = document.querySelector(".input-area");
-
-const addNewNote = () => {
-  // let newName = prompt("Name?");
-
+const openPopup = () => {
   inputArea.style.display = "flex";
 };
 
@@ -150,8 +170,28 @@ const closePopup = () => {
   inputArea.style.display = "none";
 };
 
+const saveNote = () => {
+  data = [...data];
+
+  data.push({
+    name: noteName.value,
+    created: new Date().toLocaleDateString(),
+    // надо поправить формат даты
+    category: category.value,
+    content: noteContent.value,
+    // надо сделать парсер дат
+    dates: "",
+  });
+  showDataNode(data[data.length - 1]);
+  console.log(data[data.length - 1].created);
+  noteName.value = "";
+  noteContent.value = "";
+  closePopup();
+};
+
 // EVENT LISTENERS
 document.addEventListener("DOMContentLoaded", showStartData(startData));
-tableButtons.addEventListener("click", deleteNote);
-headerButton.addEventListener("click", addNewNote);
+tableButtons.addEventListener("click", iterateNote);
+headerButton.addEventListener("click", openPopup);
 closePopupButton.addEventListener("click", closePopup);
+saveNoteButton.addEventListener("click", saveNote);
