f7dd0711022d980054151bd9fe3405d9b97eb256
Veronika Kostenko PC <veronikostenko@gmail.com>
2022-04-11 18:23:11 +0300

    get id of the clicked tableRow

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // DATA STARTER
 const startData = [
   {
+    id: 39090,
     name: "Shopping List",
     created: "April 20, 2022",
     category: "Task",
@@ -8,6 +9,7 @@ const startData = [
     dates: "",
   },
   {
+    id: 39091,
     name: "The theory of evolution",
     created: "April 27, 2022",
     category: "Random Thought",
@@ -15,6 +17,7 @@ const startData = [
     dates: "",
   },
   {
+    id: 39092,
     name: "New Feature",
     created: "May 5, 2022",
     category: "Idea",
@@ -22,6 +25,7 @@ const startData = [
     dates: "3/5/2023, 5/5/2030",
   },
   {
+    id: 39093,
     name: "Simone Biles",
     created: "May 5, 2022",
     category: "Quote",
@@ -30,6 +34,7 @@ const startData = [
     dates: "",
   },
   {
+    id: 39094,
     name: "Books",
     created: "May 17, 2022",
     category: "Task",
@@ -37,6 +42,7 @@ const startData = [
     dates: "",
   },
   {
+    id: 39095,
     name: "Business",
     created: "May 18, 2022",
     category: "Idea",
@@ -45,6 +51,7 @@ const startData = [
     dates: "15/7/2023",
   },
   {
+    id: 39096,
     name: "Emma Watson",
     created: "May 19, 2022",
     category: "Quote",
@@ -53,11 +60,11 @@ const startData = [
   },
 ];
 
-let data = [startData];
+let data = [...startData];
 
 // Selectors
 const tableHeader = document.querySelector(".table-container");
-const tableButtons = document.querySelector(".table-container");
+// const tableButtons = document.querySelector(".table-container");
 const headerButton = document.querySelector(".header-button");
 const closePopupButton = document.querySelector(".fa-square-xmark");
 const saveNoteButton = document.querySelector(".save-note-button");
@@ -88,29 +95,31 @@ const categoryIcon = (category) => {
   return output;
 };
 
-const showDataNode = (node) => {
+const showDataNode = (note) => {
   const tableRow = document.createElement("div");
   tableRow.classList.add("table-row");
+  tableRow.dataset.noteId = `${note.id}`;
+
   const name = document.createElement("div");
   name.classList.add("name");
-  name.innerHTML = `${categoryIcon(node.category)}
-  <b>${node.name}</b>`;
+  name.innerHTML = `${categoryIcon(note.category)}
+  <b>${note.name}</b>`;
 
   const created = document.createElement("div");
   created.classList.add("created");
-  created.innerText = `${node.created}`;
+  created.innerText = `${note.created}`;
 
   const category = document.createElement("div");
   category.classList.add("category");
-  category.innerText = `${node.category}`;
+  category.innerText = `${note.category}`;
 
   const content = document.createElement("div");
   content.classList.add("content");
-  content.innerText = `${node.content}`;
+  content.innerText = `${note.content}`;
 
   const dates = document.createElement("div");
   dates.classList.add("dates");
-  dates.innerText = `${node.dates}`;
+  dates.innerText = `${note.dates}`;
 
   const tableButtons = document.createElement("div");
   tableButtons.classList.add("table-buttons");
@@ -135,45 +144,11 @@ const showStartData = (startData) => {
   });
 };
 
-const iterateNote = (e) => {
-  e.preventDefault();
-  let item = e.target;
-  const tableRow = item.closest(".table-row");
-
-  // DELETE NOTE
-  if (
-    item.classList[1] === "fa-trash" &&
-    tableRow.classList[0] === "table-row"
-  ) {
-    tableRow.remove();
-  }
-
-  // EDIT NOTE
-  else if (item.classList[1] === "fa-pen") {
-    inputArea.style.display = "flex";
-    console.log(e.target.closest(".table-row").children[0].innerText);
-    noteName.value = e.target.closest(".table-row").children[0].innerText;
-    noteContent.value = e.target.closest(".table-row").children[3].innerText;
-    category.value = e.target.closest(".table-row").children[2].innerText;
-
-    console.log(tableHeader.childNodes);
-    let data = tableHeader.childNodes;
-    console.log(typeof data);
-  }
-};
-
-const openPopup = () => {
-  inputArea.style.display = "flex";
-};
-
-const closePopup = () => {
-  inputArea.style.display = "none";
-};
-
 const saveNote = () => {
   data = [...data];
 
   data.push({
+    id: Math.floor(Math.random() * 1000000),
     name: noteName.value,
     created: new Date().toLocaleDateString(),
     // надо поправить формат даты
@@ -189,9 +164,58 @@ const saveNote = () => {
   closePopup();
 };
 
+const deleteNote = (e) => {
+  e.preventDefault();
+  let item = e.target;
+  const tableRow = item.closest(".table-row");
+
+  // DELETE NOTE
+  if (
+    item.classList[1] === "fa-trash" &&
+    tableRow.classList[0] === "table-row"
+  ) {
+    tableRow.remove();
+  }
+};
+// EDIT NOTE
+
+const editNote = (e) => {
+  e.preventDefault();
+  let item = e.target;
+  const tableRow = e.target.closest(".table-row");
+
+  // item.style.color = "red";
+  if (item.classList[1] === "fa-pen") {
+    inputArea.style.display = "flex";
+    console.log(tableRow.children[0].innerText);
+    noteName.value = tableRow.children[0].innerText;
+    noteContent.value = tableRow.children[3].innerText;
+    category.value = tableRow.children[2].innerText;
+
+    let currentNote = data.find((dat) => dat.id == tableRow.dataset.noteId);
+    console.log(currentNote);
+    // currentNote.content = noteContent.value;
+    // currentNote.name = noteName.value;
+    // currentNote.category = category.value;
+    // console.log(currentNote);
+    // tableRow.remove();
+  }
+};
+
+const openPopup = () => {
+  inputArea.style.display = "flex";
+  noteName.value = "";
+  noteContent.value = "";
+};
+
+const closePopup = () => {
+  inputArea.style.display = "none";
+};
+
 // EVENT LISTENERS
 document.addEventListener("DOMContentLoaded", showStartData(startData));
-tableButtons.addEventListener("click", iterateNote);
+tableHeader.addEventListener("click", deleteNote);
+tableHeader.addEventListener("click", editNote);
 headerButton.addEventListener("click", openPopup);
 closePopupButton.addEventListener("click", closePopup);
 saveNoteButton.addEventListener("click", saveNote);
